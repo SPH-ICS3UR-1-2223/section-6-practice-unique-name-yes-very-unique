7b4f1905ac7b4783424ca013943d7c504974ea70
Krynomi <99208590+Krynomi@users.noreply.github.com>
2022-10-12 14:31:49 -0400

    Finished Problem 1 and 2

--- src/DisplayMultiples/DisplayMultiples.java
+++ src/DisplayMultiples/DisplayMultiples.java
@@ -4,8 +4,15 @@ import java.util.Scanner;
 
 public class DisplayMultiples {
 
-    public static void main(String args[]) {
-       
-    }
+	public static void main(String args[]) {
+				
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter a number!");
+		int Number = in.nextInt();
+		
+		for (int repeat = 1; repeat < 13; repeat++) {
+			System.out.println(Number+"x"+repeat+"="+Number*repeat);
+		}
+	}
 
 }
